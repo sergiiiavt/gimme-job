2fe89d9dc1025ac01edae7db9732c32549bdde62
Sergii <77669556+sergiiiavt@users.noreply.github.com>
2026-08-18 00:15:54 +0300

    Mount vacancy filter enhancer in personal workspace

--- app/workspace/page.tsx
+++ app/workspace/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
+import { VacancyFilterEnhancer } from "../vacancy-filter-enhancer";
 import VacanciesWorkspace from "../vacancies-workspace";
 
 export default function WorkspacePage() {
-  return <VacanciesWorkspace/>;
+  return <><VacanciesWorkspace/><VacancyFilterEnhancer/></>;
 }
