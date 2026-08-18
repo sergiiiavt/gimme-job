7ad06be62f01dc44ca31588b42b57086878f5ae3
Sergii <77669556+sergiiiavt@users.noreply.github.com>
2026-08-18 13:41:13 +0300

    Make public vacancy view explicit

--- app/vacancies/page.tsx
+++ app/vacancies/page.tsx
@@ -4,5 +4,5 @@ import { VacancyFilterEnhancer } from "../vacancy-filter-enhancer";
 import VacanciesWorkspace from "../vacancies-workspace";
 
 export default function VacanciesPage() {
-  return <><VacanciesWorkspace/><VacancyFilterEnhancer/></>;
+  return <><VacanciesWorkspace mode="public"/><VacancyFilterEnhancer/></>;
 }
