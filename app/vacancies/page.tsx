d3e2ee2dcd7398140fef1e5c6b815cd363a83de2
Sergii <77669556+sergiiiavt@users.noreply.github.com>
2026-08-18 13:43:43 +0300

    Remount vacancy workspace on public view

--- app/vacancies/page.tsx
+++ app/vacancies/page.tsx
@@ -4,5 +4,5 @@ import { VacancyFilterEnhancer } from "../vacancy-filter-enhancer";
 import VacanciesWorkspace from "../vacancies-workspace";
 
 export default function VacanciesPage() {
-  return <><VacanciesWorkspace mode="public"/><VacancyFilterEnhancer/></>;
+  return <><VacanciesWorkspace key="public" mode="public"/><VacancyFilterEnhancer/></>;
 }
