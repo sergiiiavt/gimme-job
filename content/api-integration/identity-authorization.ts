67d94d078d25535eb9044f1bbb47bee1716e7138
Sergii <77669556+sergiiiavt@users.noreply.github.com>
2026-08-30 03:07:25 +0300

    Load identity and authorization markdown

---
+++ content/api-integration/identity-authorization.ts
@@ -0,0 +1,9 @@
+import markdown from "./identity-authorization.md?raw";
+import markdownUk from "./identity-authorization.uk.md?raw";
+
+export const identityAuthorization = {
+  markdown,
+  markdownUk,
+};
+
+export default identityAuthorization;
