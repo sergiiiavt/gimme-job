1fab360870045cd36c6552ef2f2b0ba0049bd399
Sergii <77669556+sergiiiavt@users.noreply.github.com>
2026-09-12 14:20:23 +0300

    Enable database playground syntax guides

--- app/playgrounds/databases/page.tsx
+++ app/playgrounds/databases/page.tsx
@@ -1,6 +1,12 @@
 import "./database-workbench.css";
+import DatabaseCodeEnhancer from "./database-code-enhancer";
 import DatabasePlayground from "./database-playground";
 
 export default function DatabasePlaygroundPage() {
-  return <DatabasePlayground/>;
+  return (
+    <>
+      <DatabasePlayground/>
+      <DatabaseCodeEnhancer/>
+    </>
+  );
 }
